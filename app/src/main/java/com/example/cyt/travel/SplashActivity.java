ba14135136b2f51848e04b6699007435ddafd0b1
cytcool <675149948@qq.com>
2018-06-01 19:33:34 +0800

    闪屏页

---
+++ app/src/main/java/com/example/cyt/travel/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.cyt.travel;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        },2000);
+    }
+}
