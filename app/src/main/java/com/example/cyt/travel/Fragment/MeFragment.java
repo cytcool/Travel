4af1654a118d71c52eed72d559e7014085f18cbd
cytcool <675149948@qq.com>
2018-06-03 12:52:05 +0800

    我的页面点击登录界面

--- app/src/main/java/com/example/cyt/travel/Fragment/MeFragment.java
+++ app/src/main/java/com/example/cyt/travel/Fragment/MeFragment.java
@@ -1,20 +1,43 @@
 package com.example.cyt.travel.Fragment;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.cyt.travel.LoginActivity;
 import com.example.cyt.travel.R;
 
+import java.util.Objects;
+
 public class MeFragment extends Fragment {
+
+    protected Button mBtnLogin;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_me,container,false);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mBtnLogin = Objects.requireNonNull(getView()).findViewById(R.id.btn_login);
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
